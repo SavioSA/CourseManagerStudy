99e3493eafc813ac1d035d3d906082a9ab2db849
Sávio Silva de Araujo <saviosilvadearaujo2019@gmail.com>
2021-01-25 18:48:27 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
+import { Error404Component } from './courses/error404/error404.component';
+import { NavbarComponent } from './courses/navbar/navbar.component';
+import { ChangePipe } from './courses/change.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
+    NavbarComponent,
+    ChangePipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }, {
+        path: '**', component: Error404Component
+      }
+    ]),
+    CourseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
